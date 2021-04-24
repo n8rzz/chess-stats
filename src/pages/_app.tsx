f851d5c319acb300d7e75b85fe5cf5a3783d4757
nate geslin <teamtomkins23@gmail.com>
2021-04-23 20:06:13 -0500

    adds semantic-ui package and stylesheet to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/destructuring-assignment */
+import 'semantic-ui-css/semantic.min.css';
 import '@/styles/global.css';
 import React, { ReactElement } from 'react';
 import { AppProps } from 'next/app';
