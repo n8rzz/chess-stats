8d0a5bcc677d2be6ced5b0b7e575edbb06ca1fd7
nate geslin <teamtomkins23@gmail.com>
2021-04-30 20:10:10 -0500

    removes Promise.all in favor of for loop because chess.com limimts concurrent requests

--- src/domain/game/games.service.ts
+++ src/domain/game/games.service.ts
@@ -2,44 +2,40 @@
 import axios, { AxiosResponse } from 'axios';
 import { flatten } from 'lodash';
 import { GameCollection } from './models/Game.collection';
-import {
-  IGame,
-  IGameArchiveListResponse,
-  IGameArchiveResponse,
-} from './games.types';
+import { IGame, IGameArchiveListResponse, IGameArchiveResponse } from './games.types';
 
 export const getArchives = async (username: string): Promise<string[]> => {
   return axios
-    .get<IGameArchiveListResponse>(
-      `https://api.chess.com/pub/player/${username}/games/archives`,
-    )
-    .then(
-      (response: AxiosResponse<IGameArchiveListResponse>) =>
-        response.data.archives,
-    );
+    .get<IGameArchiveListResponse>(`https://api.chess.com/pub/player/${username}/games/archives`)
+    .then((response: AxiosResponse<IGameArchiveListResponse>) => response.data.archives);
 };
 
 export const getGameArchiveForUrl = async (url: string): Promise<IGame[]> => {
   return axios
     .get<IGameArchiveResponse>(url)
-    .then(
-      (response: AxiosResponse<IGameArchiveResponse>) => response.data.games,
-    );
+    .then((response: AxiosResponse<IGameArchiveResponse>) => response.data.games);
 };
 
 export const getHistorcialGamesFromArchiveList = async (
   archiveList: string[],
   username: string,
 ): Promise<GameCollection> => {
-  const gameArchiveForMonthRequestList = archiveList.map(
-    (archiveUrl: string) => () => getGameArchiveForUrl(archiveUrl),
-  );
+  const gameArchiveForMonthRequestList = [];
 
-  return Promise.all([...gameArchiveForMonthRequestList.map((c) => c())]).then(
-    (response: IGame[][]) => {
-      const flatGamesResponse = flatten(response);
+  for (let i = 0; i < archiveList.length; i++) {
+    const archiveUrl = archiveList[i];
 
-      return new GameCollection(username, flatGamesResponse, 0);
-    },
-  );
+    try {
+      // eslint-disable-next-line no-await-in-loop
+      const response = await getGameArchiveForUrl(archiveUrl);
+
+      gameArchiveForMonthRequestList.push(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const flatGameList = flatten(gameArchiveForMonthRequestList);
+
+  return Promise.resolve(new GameCollection(username, flatGameList, flatGameList.length));
 };
