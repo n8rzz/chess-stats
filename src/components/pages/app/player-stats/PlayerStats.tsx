97f8d7f4bf7203ac27fa8535f1b6472f780c06d4
nate geslin <teamtomkins23@gmail.com>
2021-08-03 18:13:32 -0500

    removes GameTypeStats component and moves logic up to PlayerStats

--- src/components/pages/app/player-stats/PlayerStats.tsx
+++ src/components/pages/app/player-stats/PlayerStats.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import clsx from 'clsx';
-import { Dimmer, Loader, Segment } from 'semantic-ui-react';
+import dynamic from 'next/dynamic';
+import { Card, Dimmer, Loader, Segment } from 'semantic-ui-react';
 import styles from '../../../../styles/App.module.css';
 import { IChessStats, IHighLow } from '../../../../domain/player/player.types';
-import { GameTypeStats } from '../../../shared/game-type-stats/GameTypeStats';
+import { setDateFromUtcSeconds } from '../../../../util/date.utils';
+
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
 interface IProps {
   highLow: IHighLow;
@@ -29,7 +32,57 @@ export const PlayerStats: React.FC<IProps> = (props) => {
 
   return (
     <div className={clsx(styles.container, styles.vr3)}>
-      <GameTypeStats isLoading={props.isLoading} label={'Rapid'} highLow={props.highLow} stats={props.stats} />
+      <Card.Group itemsPerRow={2} stackable={true}>
+        <Card>
+          <Card.Content>
+            <Card.Header as={'h3'}>Rapid</Card.Header>
+            <Card.Description>
+              <Chart
+                series={[props.stats.record.win, props.stats.record.loss, props.stats.record.draw]}
+                options={{
+                  dataLabels: {
+                    enabled: false,
+                  },
+                  labels: ['win', 'loss', 'draw'],
+                  legend: {
+                    position: 'bottom',
+                    horizontalAlign: 'center',
+                  },
+                }}
+                type={'donut'}
+                height={225}
+              />
+            </Card.Description>
+          </Card.Content>
+        </Card>
+        <Card>
+          <Card.Content>
+            <Card.Header>{props.stats.best.rating}</Card.Header>
+            <Card.Meta>best</Card.Meta>
+          </Card.Content>
+          <Card.Content extra={true}>{setDateFromUtcSeconds(props.stats.best.date).toLocaleString()}</Card.Content>
+        </Card>
+        <Card>
+          <Card.Content>
+            <Card.Header>{props.highLow.lowest.rating}</Card.Header>
+            <Card.Meta>tactics</Card.Meta>
+            <Card.Description>
+              <b>lowest</b>
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra={true}>{setDateFromUtcSeconds(props.highLow.lowest.date).toLocaleString()}</Card.Content>
+        </Card>
+        <Card>
+          <Card.Content>
+            <Card.Header>{props.highLow.highest.rating}</Card.Header>
+            <Card.Meta>tactics</Card.Meta>
+            <Card.Description>
+              <b>highest</b>
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra={true}>{setDateFromUtcSeconds(props.highLow.highest.date).toLocaleString()}</Card.Content>
+        </Card>
+      </Card.Group>
     </div>
   );
 };
